333a9accbb98878e071743c9445c10778dde7c02
BugHunter-X <askzenai@gmail.com>
2025-01-15 10:56:05 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,21 @@
+```go
+func main() {
+
+
+        var i int
+        fmt.Println(i)
+        for j := 0; j < 5; j++ {
+                i++
+        }
+        fmt.Println(i)
+
+
+        var k int
+        fmt.Println(k)
+        for j := 0; j < 5; j++ {
+                k = k +1
+        }
+        fmt.Println(k)
+
+}
+```
